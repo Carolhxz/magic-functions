f0d101a316abba331f1560bc3aa7d030106cfdbf
maila520 <mailasantos520@gmail.com>
2024-03-28 20:18:28 -0300

    desafio 12 ok

--- src/averageRatingByGenre.js
+++ src/averageRatingByGenre.js
@@ -1,8 +1,35 @@
 const movies = require("../data/movies");
-function averageRatingByGenre(genero) {
 
+function averageRatingByGenre(genero) {
+  if (genero === undefined) {
+    return "O parâmetro é obrigatório.";
+  }
+  if (typeof genero !== "string") {
+    return "O parâmetro tem que ser uma string.";
+  }
+  const genremovies = movies.filter(movie => movie.genero.includes(genero));
+  //console.log(genremovies);
+  const avaliações = movies.map(movie => movie.avaliacao);
+  
+  //console.log(avaliações); 
+  if (genremovies.length === 0) {
+    return `Não há filmes desse genero ${genero}`;
+  }
+  const avaliaçãototal = genremovies.reduce((sum, movie) => sum + movie.avaliacao, 0);
+
+  //console.log(avaliaçãototal);
+  
+  const avaliação = avaliaçãototal / genremovies.length;
+
+  //console.log(avaliação);
+  return avaliação.toFixed(2);
 }
 
-module.exports = { 
+const genero = "Ação"; 
+const avaliação2 = averageRatingByGenre(genero);
+
+console.log(`Média de avaliações para ${genero}: ${avaliação2}`);
+
+module.exports = {
   averageRatingByGenre
 }
