9992147296a60b794aa3954304742a4661afdef2
maila520 <mailasantos520@gmail.com>
2024-04-01 15:53:13 -0300

    desafio 13 ok

--- src/listOverdueLoans.js
+++ src/listOverdueLoans.js
@@ -1,8 +1,32 @@
 const movies = require("../data/movies");
 
 function listOverdueLoans(movies) {
- 
+
+  const data = new Date(); 
+
+  const vencidos = [];
+
+  for (const movie of movies) {
+    if (movie.emprestimos) { 
+      for (const loan of movie.emprestimos) {
+        const date = new Date(loan.dataDevolucao); 
+        if (date < data) { 
+          vencidos.push({
+            id: movie.id,
+            titulo: movie.titulo,
+            dataEmprestimo: loan.dataEmprestimo,
+            dataDevolucao: loan.dataDevolucao,
+            nomePessoa: loan.nomePessoa,
+          });
+        }
+      }
+    }
+  }
+
+  return vencidos;
 }
+
+console.log(listOverdueLoans(movies));
 
 module.exports = {
   listOverdueLoans
