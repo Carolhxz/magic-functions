ac30a2024a461f9724e291e7863f9e4d42074dcd
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-04-01 16:58:28 -0300

    Desafio classifyMovies completo

--- src/classifyMovies.js
+++ src/classifyMovies.js
@@ -1,7 +1,28 @@
 const movies = require("../data/movies");
 
 function classifyMovies(movies) {
- 
+  const categorizedMovies = movies.reduce((acc, {avaliacao, disponivel, titulo, diretor, genero, anoLancamento})=> {
+    let interval
+
+    if (avaliacao >= 0 && avaliacao < 5.0) {
+      interval = '0-4.9'
+    } else if (avaliacao >= 5.0 && avaliacao < 8.0){
+      interval = '5.0-7.9'
+    } else {
+      interval = '8.0-10.0'
+    }
+
+    acc.avaliacao[interval].push({titulo, diretor, genero, anoLancamento, avaliacao})
+    disponivel ? acc.disponibilidade.disponiveis.push({titulo}) : acc.disponibilidade.naoDisponiveis.push({titulo})
+    
+    return acc
+  }, {avaliacao: {'0-4.9': [], '5.0-7.9': [], '8.0-10.0': []}, disponibilidade: {disponiveis: [], naoDisponiveis: []}})
+
+  for (const interval in categorizedMovies.avaliacao) {
+    categorizedMovies.avaliacao[interval].sort((a,b)=> a.titulo.localeCompare(b.titulo))
+  }
+
+  return categorizedMovies
 }
 
 module.exports = {
