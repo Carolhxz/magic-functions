c8754e2dd4de5e39606ffe7b462893d0d7a5b94f
maila520 <mailasantos520@gmail.com>
2024-04-01 12:32:44 -0300

    desafio 12 ok

--- src/filmSeparation.js
+++ src/filmSeparation.js
@@ -1,9 +1,35 @@
 const movies = require("../data/movies");
 
 function filmSeparation(movies, details = true) {
- 
+  const filmes = {};
+  const uniqueGenres = new Set();
+
+  movies.forEach(movie => {
+    movie.genero.forEach(genero => {
+      uniqueGenres.add(genero);
+      if (!filmes[genero]) {
+        filmes[genero] = [];
+      }
+      if (details) {
+        filmes[genero].push(movie.titulo);
+      } else {
+        filmes[genero].push(movie);
+      }
+    });
+  });
+
+  for (let key in filmes) {
+    if (details) {
+      filmes[key] = filmes[key].sort((a, b) => a.localeCompare(b.titulo));
+    } else {
+      filmes[key] = filmes[key].sort();
+    }
+  }
+
+  return filmes;
 }
 
+console.log(filmSeparation(movies));
 module.exports = {
   filmSeparation
 }
