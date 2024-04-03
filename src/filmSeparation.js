7f5a7a25346aa01a303cb657ee0a7908a9cc2c53
Wellington Farias <tonw2farias@gmail.com>
2024-04-03 19:20:34 -0300

    Requisito 14

--- src/filmSeparation.js
+++ src/filmSeparation.js
@@ -1,7 +1,34 @@
 const movies = require("../data/movies");
 
 function filmSeparation(movies, details = true) {
- 
+  const generosEFilmes = {}
+
+  for (const movie of movies) {
+    if (movie.genero?.length > 0) {
+      for (const genre of movie.genero) {
+        generosEFilmes[genre] = generosEFilmes[genre] || []
+        const itemDoArray = details ? {
+          anoLancamento: movie.anoLancamento,
+          avaliacao: movie.avaliacao,
+          diretor: movie.diretor,
+          id: movie.id,
+          titulo: movie.titulo
+        } : movie.titulo
+        generosEFilmes[genre].push(itemDoArray)
+      }
+    }
+  }
+
+  const generosEFilmesOrdenados = {}
+  for (const genero of Object.keys(generosEFilmes).sort()) {
+    generosEFilmesOrdenados[genero] = details ?
+      generosEFilmes[genero].sort((a, b) => a.titulo.localeCompare(b.titulo)) :
+      generosEFilmes[genero].sort()
+  }
+
+  return generosEFilmesOrdenados
+
+
 }
 
 module.exports = {
--- src/listOverdueLoans.js
+++ src/listOverdueLoans.js
@@ -1,7 +1,28 @@
 const movies = require("../data/movies");
 
 function listOverdueLoans(movies) {
- 
+  const currentDate = new Date()
+  const overdueLoans = []
+
+  for (const movie of movies) {
+    if (movie.emprestimos?.length > 0) {
+      for (const loan of movie.emprestimos) {
+        if (new Date(loan.dataDevolucao) < currentDate) {
+          loan.status = "atrasado"
+          overdueLoans.push({
+            id: movie.id,
+            titulo: movie.titulo,
+            dataEmprestimo: loan.dataEmprestimo,
+            dataDevolucao: loan.dataDevolucao,
+            nomePessoa: loan.nomePessoa
+          })
+        }
+      }
+    }
+  }
+
+  return overdueLoans
+
 }
 
 module.exports = {
