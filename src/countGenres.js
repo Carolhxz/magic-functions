a18b448d96a3691efcc1de71151f1f73c3693ee0
Wellington Farias <tonw2farias@gmail.com>
2024-04-03 18:45:44 -0300

    Requisito 10

--- src/countGenres.js
+++ src/countGenres.js
@@ -1,7 +1,26 @@
 const movies = require("../data/movies");
 
 function countGenres(movies) {
- 
+
+
+  if (movies.length === 0) {
+    return {};
+  }
+  const genreCounts = {}
+
+  movies.forEach(movie => {
+    if (movie.genero && movie.genero.length > 0) {
+      movie.genero.forEach(genero => {
+        if (genreCounts.hasOwnProperty(genero)) {
+          genreCounts[genero]++;
+        }
+        else {
+          genreCounts[genero] = 1
+        }
+      })
+    }
+  });
+  return genreCounts
 }
 
 module.exports = {
