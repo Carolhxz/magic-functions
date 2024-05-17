491402961406990d187a7896c9942e1d940dd437
Ariovaldo Sant'Ana <153957898+AriovaldoSantS@users.noreply.github.com>
2024-05-17 01:25:13 -0300

    Desafio09

--- src/findActorForTheMovie.js
+++ src/findActorForTheMovie.js
@@ -1,7 +1,13 @@
 const movies = require("../data/movies");
 
 const findActorForTheMovie = (titulo) => {
-  
+  if (!titulo) return "Filme não encontrado.";
+
+  const filme = movies.find(item => item.titulo.toUpperCase() === titulo.toUpperCase());
+
+  if (!filme) return "Filme não encontrado.";
+
+  return filme.elenco.slice(0, 2);
 }
 
 module.exports = {
