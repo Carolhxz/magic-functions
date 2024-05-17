20c1c10072bbc53b802f8825f91fc9a275c38f02
Ariovaldo Sant'Ana <153957898+AriovaldoSantS@users.noreply.github.com>
2024-05-17 01:17:34 -0300

    desafio8

--- src/nolanFilms.js
+++ src/nolanFilms.js
@@ -1,6 +1,11 @@
 const movies = require("../data/movies");
 
 const nolanFilms = (movies) => {
+  const nolan = movies
+  .filter(movie => movie.diretor === 'Christopher Nolan')
+  .sort((a, b) => a - b);
+
+  return nolan
  
 }
 
