2123160799fad18fb235aa14b05b2df8b62b9e96
Ariovaldo Sant'Ana <153957898+AriovaldoSantS@users.noreply.github.com>
2024-05-17 01:11:59 -0300

    Desafio7

--- src/filmsAvailable.js
+++ src/filmsAvailable.js
@@ -1,8 +1,11 @@
 const movies = require("../data/movies");
 
 const filmsAvailable = (movies) => {
+ return movies.filter((element) => element.disponivel == true).map((element) => element.titulo)
  
 }
+
+
 
 module.exports = {
   filmsAvailable
