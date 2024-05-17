023bc65bb96170aec5624cf19b853cab131bdae7
Ariovaldo Sant'Ana <153957898+AriovaldoSantS@users.noreply.github.com>
2024-05-17 00:56:48 -0300

    desafio04

--- src/bestRatedFilm.js
+++ src/bestRatedFilm.js
@@ -1,8 +1,17 @@
 const movies = require("../data/movies");
 
 const bestRatedFilm = (movies) => {
-  
+  let bestRated = movies[0];
+
+  movies.forEach(movie => {
+    if (movie.avaliacao > bestRated.avaliacao) {
+      bestRated = movie;
+    }
+  });
+
+  return bestRated;
 }
+
 
 module.exports = {
   bestRatedFilm
