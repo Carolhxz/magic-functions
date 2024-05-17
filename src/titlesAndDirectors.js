c20e3d0c9c6c92b1c29e2a35d71dda4c4dbaeead
Ariovaldo Sant'Ana <153957898+AriovaldoSantS@users.noreply.github.com>
2024-05-17 01:02:25 -0300

    desafio5

--- src/titlesAndDirectors.js
+++ src/titlesAndDirectors.js
@@ -1,7 +1,9 @@
 const { movies } = require("../data/list1");
 
 const titlesAndDirectors = (movies) => {
- 
+  return movies.forEach(({ titulo, diretor }) => {
+    console.log(`Título: ${titulo} - Diretor: ${diretor}`);
+  });
 }
 
 module.exports = {
