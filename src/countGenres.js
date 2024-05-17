0894c0795b711335eb2ee36209e198209644666f
Ariovaldo Sant'Ana <153957898+AriovaldoSantS@users.noreply.github.com>
2024-05-17 01:28:10 -0300

    desafio10

--- src/countGenres.js
+++ src/countGenres.js
@@ -1,7 +1,16 @@
 const movies = require("../data/movies");
 
 function countGenres(movies) {
- 
+  if (movies.length === 0) return {};
+
+  return movies.reduce((contGenero, movie) => {
+    if (movie.genero && movie.genero.length > 0) {
+      movie.genero.forEach(genero => {
+        contGenero[genero] = (contGenero[genero] || 0) + 1;
+      });
+    }
+    return contGenero;
+  }, {});
 }
 
 module.exports = {
