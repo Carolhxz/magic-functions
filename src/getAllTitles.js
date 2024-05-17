c1902fce675535d7c04dde4e8dc2cb99beb650fe
Ariovaldo Sant'Ana <153957898+AriovaldoSantS@users.noreply.github.com>
2024-05-17 00:04:49 -0300

    desafio1

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,7 +1,11 @@
 const movies = require("../data/movies");
 
 const getAllTitles = (movies) => {
- 
+ if (!movies || movies.length === 0 ) {
+  return []
+ }
+
+ return movies.map(movie => movie.titulo)
 }
 
 module.exports = {
